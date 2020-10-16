1a6d25382f65669f0ba1e28aa8415b2b37a40615
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-16 15:17:29 +0000

    Commit 6

--- Customer.java
+++ Customer.java
@@ -24,11 +24,8 @@ public class Customer {
    String result = "Rental Record for " + getName() + "\n";
    while (rentals.hasMoreElements()) {
       Rental each = (Rental) rentals.nextElement();
-      // add frequent renter points
-      frequentRenterPoints ++;
-      // add bonus for a two day new release rental
-      if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-         each.getDaysRented() > 1) frequentRenterPoints ++;
+      
+      frequentRenterPoints += each.getFrequentRenterPoints();
 
       // show figures for this rental
       result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(each.getCharge()) + "\n";
--- Rental.java
+++ Rental.java
@@ -32,4 +32,12 @@ public class Rental {
     }
     return thisAmount;
   }
+
+  public int getFrequentRenterPoints() {
+    if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+      return 2;
+    else
+      return 1;
+  }
+
 }
