78725364ab72505c5f4ebbd60638a01ab55cbe20
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-16 16:44:36 +0000

    Commit 14

--- Movie.java
+++ Movie.java
@@ -39,11 +39,8 @@ public class Movie {
   public double getCharge(int daysRented) {
     return _price.getCharge(daysRented);
   }
-  
+
   public int getFrequentRenterPoints(int daysRented){
-    if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-      return 2;
-    else
-      return 1;
+    return _price.getFrequentRenterPoints(daysRented);
   }
 }
