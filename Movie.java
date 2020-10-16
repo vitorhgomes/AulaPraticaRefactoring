547eea2c5c722a93294a0585ec6497c1bdf7a637
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-16 16:28:48 +0000

    Commit 9

--- Movie.java
+++ Movie.java
@@ -23,4 +23,21 @@ public class Movie {
   public String getTitle (){
       return _title;
   }
+
+  public double getCharge(int daysRented){
+    double thisAmount = 0;
+    switch (getPriceCode()) {
+    case Movie.REGULAR:
+      thisAmount += 2;
+      if (daysRented > 2)
+        thisAmount += (daysRented - 2) * 1.5;
+    case Movie.NEW_RELEASE:
+      thisAmount += daysRented * 3;
+    case Movie.CHILDRENS:
+      thisAmount += 1.5;
+      if (daysRented > 3)
+        thisAmount += (daysRented - 3) * 1.5;
+    }
+    return thisAmount;
+  }
 }
