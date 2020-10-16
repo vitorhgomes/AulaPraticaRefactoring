9fed2af029edafb7d764393e61409de4fa963bf6
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-16 14:52:49 +0000

    Commit 2

--- Customer.java
+++ Customer.java
@@ -16,6 +16,23 @@ public class Customer {
    public String getName (){
       return _name;
    }
+
+  public double amountFor(Rental each) {
+    double thisAmount = 0;
+    switch (each.getMovie().getPriceCode()) {
+    case Movie.REGULAR:
+      thisAmount += 2;
+      if (each.getDaysRented() > 2)
+        thisAmount += (each.getDaysRented() - 2) * 1.5;
+    case Movie.NEW_RELEASE:
+      thisAmount += each.getDaysRented() * 3;
+    case Movie.CHILDRENS:
+      thisAmount += 1.5;
+      if (each.getDaysRented() > 3)
+        thisAmount += (each.getDaysRented() - 3) * 1.5;
+    }
+    return thisAmount;
+  }
   
   public String statement() {
      double totalAmount = 0;
@@ -27,21 +44,7 @@ public class Customer {
         Rental each = (Rental) rentals.nextElement();
 
         //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
+        thisAmount = amountFor(each);
 
         // add frequent renter points
         frequentRenterPoints ++;
