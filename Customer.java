c2928beed2e03903342992c1f19fce76ccab2616
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-16 14:56:43 +0000

    Commit 3

--- Customer.java
+++ Customer.java
@@ -17,19 +17,19 @@ public class Customer {
       return _name;
    }
 
-  public double amountFor(Rental each) {
+  public double amountFor(Rental aRental) {
     double thisAmount = 0;
-    switch (each.getMovie().getPriceCode()) {
+    switch (aRental.getMovie().getPriceCode()) {
     case Movie.REGULAR:
       thisAmount += 2;
-      if (each.getDaysRented() > 2)
-        thisAmount += (each.getDaysRented() - 2) * 1.5;
+      if (aRental.getDaysRented() > 2)
+        thisAmount += (aRental.getDaysRented() - 2) * 1.5;
     case Movie.NEW_RELEASE:
-      thisAmount += each.getDaysRented() * 3;
+      thisAmount += aRental.getDaysRented() * 3;
     case Movie.CHILDRENS:
       thisAmount += 1.5;
-      if (each.getDaysRented() > 3)
-        thisAmount += (each.getDaysRented() - 3) * 1.5;
+      if (aRental.getDaysRented() > 3)
+        thisAmount += (aRental.getDaysRented() - 3) * 1.5;
     }
     return thisAmount;
   }
