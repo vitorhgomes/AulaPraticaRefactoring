20d49f1ad22d343106e0a67e14f0cc338c2e0a11
Vitor Gomes <vitorgomes.ufmg@gmail.com>
2020-10-16 16:24:57 +0000

    Commit 8

--- Customer.java
+++ Customer.java
@@ -2,20 +2,35 @@ import java.util.Enumeration;
 import java.util.Vector;
 
 public class Customer {
-   private String _name;
-   private Vector _rentals = new Vector();
+  private String _name;
+  private Vector _rentals = new Vector();
 
-   public Customer (String name){
-      _name = name;
-   }
+  public Customer (String name){
+    _name = name;
+  }
 
-   public void addRental(Rental arg) {
-      _rentals.addElement(arg);
-   }
+  public void addRental(Rental arg) {
+    _rentals.addElement(arg);
+  }
    
-   public String getName (){
-      return _name;
-   }
+  public String getName (){
+    return _name;
+  }
+
+  public String htmlStatement() {
+    Enumeration rentals = _rentals.elements();
+    String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+    while (rentals.hasMoreElements()) {
+      Rental each = (Rental) rentals.nextElement();
+      // show figures for each rental
+      result += each.getMovie().getTitle()+ ": " + String.valueOf(each.getCharge()) + "<BR>\n";
+    }
+    
+    // add footer lines
+    result +=  "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+    result += "On this rental you earned <EM>" + String.valueOf(getTotalFrequentRenterPoints()) + "</EM> frequent renter points<P>";
+    return result;
+  }
   
   public String statement() {
     Enumeration rentals = _rentals.elements();
